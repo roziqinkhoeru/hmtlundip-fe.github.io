e27559dbcd021035e915f29718ea27ccda0c4fb7
RafliFerdian25 <ferdianrafli25@gmail.com>
2021-11-28 17:01:38 +0700

    berita

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -262,6 +262,30 @@ $(document).ready(function () {
   });
 });
 
+// berita - berita-terkini-content
+$(document).ready(function () {
+  $(".owl-carousel#berita-terkini-content").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    dots: true,
+    autoplay: true,
+    autoplayTimeout: 6000,
+    smartSpeed: 1000,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 1,
+      },
+      1000: {
+        items: 1,
+      },
+    },
+  });
+});
+
 // contact - social media section
 $(document).ready(function () {
   $(".owl-carousel#socmed-content").owlCarousel({
